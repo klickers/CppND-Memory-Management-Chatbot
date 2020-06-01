b8782ae82b2165303f32282d4b05d003a3ab8877
root <bluebirid228@gmail.com>
2020-06-01 00:46:52 +0000

    Task 2 almost done

--- src/chatbot.h
+++ src/chatbot.h
@@ -29,10 +29,10 @@ public:
 
     //// STUDENT CODE
     ////
-	Chatbot(const ChatBot &source);             // copy constructor
+	ChatBot(const ChatBot &source);             // copy constructor
     ChatBot &operator=(const ChatBot &source);  // copy assignment operator
-    Chatbot(Chatbot &&source);                  // move constructor
-    Chatbot &operator=(Chatbot &&source);       // move assignment operator
+    ChatBot(ChatBot &&source);                  // move constructor
+    ChatBot &operator=(ChatBot &&source);       // move assignment operator
     ////
     //// EOF STUDENT CODE
 
